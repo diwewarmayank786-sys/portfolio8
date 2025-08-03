f8628296b62604d5c1e4585779f4014e83733127
diwewarmayank786-sys <diwewarmayank786@gmail.com>
2025-08-03 18:54:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Just a basic script for form submission alert
+document.querySelector('.contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thanks for your message! I will get back to you soon.');
+});
